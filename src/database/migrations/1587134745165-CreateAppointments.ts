f924f2af8a0bc3fe622973e664dcdcdc7b36b572
Joao Lucas <jlmelosouza18@gmail.com>
2020-04-20 11:29:01 -0300

    feat: relationships

--- src/database/migrations/1587134745165-CreateAppointments.ts
+++ src/database/migrations/1587134745165-CreateAppointments.ts
@@ -8,7 +8,7 @@ class CreateAppointments1587088270086 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
@@ -16,12 +16,10 @@ class CreateAppointments1587088270086 implements MigrationInterface {
           {
             name: 'provider',
             type: 'varchar',
-            isNullable: false,
           },
           {
             name: 'date',
             type: 'timestamp with time zone',
-            isNullable: false,
           },
           {
             name: 'created_at',

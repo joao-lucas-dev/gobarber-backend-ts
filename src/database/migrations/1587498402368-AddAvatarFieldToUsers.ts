df4a01c82ba4bd0e9d4a5e7a65fedc45cb0a0c57
Joao Lucas <jlmelosouza18@gmail>
2020-04-21 17:19:54 -0300

    feat: avatar

---
+++ src/database/migrations/1587498402368-AddAvatarFieldToUsers.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export default class AddAvatarFieldToUsers1587498402368
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'users',
+      new TableColumn({
+        name: 'avatar',
+        type: 'varchar',
+        isNullable: true,
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.dropColumn('users', 'avatar');
+  }
+}
